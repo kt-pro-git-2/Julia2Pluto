4fd332fffbe528df9d20b2765c6d8656fd734f33
kt-pro-git-2 <127727685+kt-pro-git-2@users.noreply.github.com>
2023-10-04 16:12:39 +0900

    Create 講座の内容.jl

---
+++ calculus/講座の内容.jl
@@ -0,0 +1,32 @@
+### A Pluto.jl notebook ###
+# v0.19.29
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e6fd1ee0-575c-11ee-3ff3-79f51b1d05ef
+md"""
+
+[関数,関数のグラフ](FunGra.html)
+
+[関数の変換：グラフの移動，拡大縮小](Trans.html)
+
+[関数の極値](Limit.html)
+
+[逆関数]()
+[対数関数]()
+
+[三角関数]()
+
+[関数の微分](di.html)
+
+[関数の定積分](di.html)
+
+[不定積分]()
+
+
+
+"""
+
+# ╔═╡ Cell order:
+# ╠═e6fd1ee0-575c-11ee-3ff3-79f51b1d05ef
